f44e74938399364a9eeb20644b0f1d51752254aa
yoohwa-cho <43ryuflower@gmail.com>
2022-08-30 22:58:40 +0900

    Runtime: 6 ms, faster than 9.59% / Memory Usage: 6.6 MB, less than 32.63%

---
+++ LeetCode/Study Plan/Data Structure/Array_118_Pascal_Triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pascalTriangle;
+        for(int i=0;i<numRows; i++){
+            vector<int> tmpRow(i+1,0);
+            for(int j=0;j<i+1;j++){
+                if(j==0 || j==i) tmpRow[j] = 1;
+                else {
+                    tmpRow[j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j];
+                }
+            }
+            pascalTriangle.push_back(tmpRow);
+        }
+        return pascalTriangle;
+    }
+};
