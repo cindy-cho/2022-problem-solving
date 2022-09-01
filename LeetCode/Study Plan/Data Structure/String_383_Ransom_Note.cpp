11fb411c5fcb54430fe14193b1f5434cdd720dfa
yoohwa-cho <43ryuflower@gmail.com>
2022-09-01 23:07:31 +0900

    Runtime: 8 ms, faster than 97.75% / Memory Usage: 8.6 MB, less than 89.99%

--- LeetCode/Study Plan/Data Structure/String_383_Ransom_Note.cpp
+++ LeetCode/Study Plan/Data Structure/String_383_Ransom_Note.cpp
@@ -1,3 +1,4 @@
+//  Runtime: 43 ms, faster than 17.84% / Memory Usage: 8.9 MB, less than 40.12%
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
@@ -9,6 +10,25 @@ public:
             magazineCount[i] = count(magazine.begin(), magazine.end(), tmp);
             if(ransomCount[i] > magazineCount[i]) return false;
         }
+        
+        return true;
+    }
+};
+
+// Runtime: 8 ms, faster than 97.75% / Memory Usage: 8.6 MB, less than 89.99%
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> ransomCount (26,0), magazineCount(26,0);
+        for(auto ch : ransomNote){
+            ransomCount[ch-'a']++;
+        }
+        
+        for(auto ch : magazine){
+            magazineCount[ch-'a']++;
+        }
+        
+        for(int i=0;i<26;i++) if(ransomCount[i] > magazineCount[i]) return false;
         
         return true;
     }
